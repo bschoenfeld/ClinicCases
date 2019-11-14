473a6c98b01e1d50138958f8eea0a86081b30f73
Ben Schoenfeld <ben.schoenfeld@portsolutionintegrators.com>
2019-11-14 10:03:09 -0500

    Implement add and delete functions. Remove old code

---
+++ pp/delete.php
@@ -0,0 +1,24 @@
+
+<?php
+
+session_start();
+require('../lib/php/auth/session_check.php');
+require('../db.php');
+require('api.php');
+
+try {
+    // Connect to PP
+    $ppApiConfig = getApiConfig($dbh);
+    $ppAccountsApi = getPpAccountsApi($ppApiConfig);
+    
+    $ppAccountsApi->accountsDelete($_POST['accountId']);
+
+    header('Content-type: application/json');
+    echo json_encode(array('success'=>true));
+
+} catch(Exception $e) {
+	//400 is sent to trigger an error for ajax requests.
+    header('HTTP/1.1 400 Bad Request');
+
+    echo $e->getMessage();
+}
